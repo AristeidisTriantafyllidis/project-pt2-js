a7977910435c63b199e752eb5fc7e0402e7ee4ad
Aristeidis Triantafyllidis <tria.aris@gmail.com>
2026-04-25 10:08:05 +0300

    final

--- form validation/index.js
+++ form validation/index.js
@@ -2,23 +2,23 @@ const name = document.getElementById("name");
 const submit = document.getElementById("submit");
 const display = document.getElementById("display");
 const email = document.getElementById("email");
+const form = document.getElementById("form")
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const checkerEmail = email.value;
+    const textName = name.value;
 
-submit.addEventListener("click",() =>{
-const checkerEmail= email.value;
-    const textName=name.value;
+    if (isNaN(textName) && checkerEmail !== "") {
 
-if (isNaN(textName) && checkerEmail!=="" ){
+        display.textContent = "Your form sumbited";
+    }
+    else {
 
-    display.textContent="Your form sumbited";
-}
-else{
-
-    display.textContent="FAILED";
-}
+        display.textContent = "FAILED";
+    }
 
 
 
 
 })
-//maybe regex for debug?
